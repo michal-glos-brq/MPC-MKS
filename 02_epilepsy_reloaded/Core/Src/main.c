d9363cbeae807d3068c0e3a3811b9dd171f0c64d
michal-glos-brq <michal.glos.brno@gmail.com>
2024-10-02 15:29:10 +0200

    ddsd

--- 02_epilepsy_reloaded/Core/Src/main.c
+++ 02_epilepsy_reloaded/Core/Src/main.c
@@ -37,6 +37,10 @@
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
 #define BLINK_TIME 300
+#define BUTTON_PERIOD 40
+#define BUTTON_PERIOD_SHORT 5
+#define LED_TIME_SHORT 100
+#define LED_TIME_LONG 1000
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
@@ -67,8 +71,57 @@ static void blink() {
 	static uint32_t last_blink_tick;
 
 	if (Tick > last_blink_tick + BLINK_TIME) {
-		last_blink_tick += Tick;
+		last_blink_tick = Tick;
 		LL_GPIO_TogglePin(LED1_GPIO_Port, LED1_Pin);
+	}
+
+}
+
+static void button2() {
+	static uint16_t states = 0xFFFF;
+	static uint32_t off_time = 0xFFFFFFFF;
+
+	uint32_t s2 = LL_GPIO_IsInputPinSet(S2_GPIO_Port, S2_Pin);
+	states <<= 1;
+
+	if (s2) {
+		states |= (uint16_t)1;
+	}
+
+	if (states == 0x8000) {
+		LL_GPIO_SetOutputPin(LED2_GPIO_Port, LED2_Pin);
+		off_time = Tick;
+	}
+
+	if (Tick > off_time + LED_TIME_LONG) {
+		LL_GPIO_ResetOutputPin(LED2_GPIO_Port, LED2_Pin);
+	}
+}
+
+static void button() {
+	static uint32_t off_time = 0xFFFFFFFF;
+
+	static uint32_t old_s1 = 1;
+	static uint32_t old_s2 = 1;
+
+	uint32_t new_s1 = LL_GPIO_IsInputPinSet(S1_GPIO_Port, S1_Pin);
+	uint32_t new_s2 = LL_GPIO_IsInputPinSet(S2_GPIO_Port, S2_Pin);
+
+
+	if (old_s2 && !new_s2) {
+		off_time = Tick + LED_TIME_LONG;
+		LL_GPIO_SetOutputPin(LED2_GPIO_Port, LED2_Pin);
+	}
+	old_s2 = new_s2;
+
+	if (old_s1 && !new_s1) {
+		off_time = Tick + LED_TIME_SHORT;
+		LL_GPIO_SetOutputPin(LED2_GPIO_Port, LED2_Pin);
+	}
+	old_s1 = new_s1;
+
+	if (Tick > off_time) {
+		LL_GPIO_ResetOutputPin(LED2_GPIO_Port, LED2_Pin);
 	}
 }
 
@@ -110,6 +163,12 @@ int main(void)
   {
     /* USER CODE END WHILE */
 	  blink();
+
+	  static uint32_t last_button_tick;
+	  if (Tick > last_button_tick + BUTTON_PERIOD_SHORT) {
+		  last_button_tick = Tick;
+		  button2();
+	  }
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
@@ -245,19 +304,10 @@ static void MX_GPIO_Init(void)
   LL_SYSCFG_SetEXTISource(LL_SYSCFG_EXTI_PORTC, LL_SYSCFG_EXTI_LINE13);
 
   /**/
-  LL_SYSCFG_SetEXTISource(LL_SYSCFG_EXTI_PORTC, LL_SYSCFG_EXTI_LINE0);
-
-  /**/
   LL_GPIO_SetPinPull(B1_GPIO_Port, B1_Pin, LL_GPIO_PULL_NO);
 
   /**/
-  LL_GPIO_SetPinPull(S2_GPIO_Port, S2_Pin, LL_GPIO_PULL_UP);
-
-  /**/
   LL_GPIO_SetPinMode(B1_GPIO_Port, B1_Pin, LL_GPIO_MODE_INPUT);
-
-  /**/
-  LL_GPIO_SetPinMode(S2_GPIO_Port, S2_Pin, LL_GPIO_MODE_INPUT);
 
   /**/
   EXTI_InitStruct.Line_0_31 = LL_EXTI_LINE_13;
@@ -267,11 +317,10 @@ static void MX_GPIO_Init(void)
   LL_EXTI_Init(&EXTI_InitStruct);
 
   /**/
-  EXTI_InitStruct.Line_0_31 = LL_EXTI_LINE_0;
-  EXTI_InitStruct.LineCommand = ENABLE;
-  EXTI_InitStruct.Mode = LL_EXTI_MODE_IT;
-  EXTI_InitStruct.Trigger = LL_EXTI_TRIGGER_FALLING;
-  LL_EXTI_Init(&EXTI_InitStruct);
+  GPIO_InitStruct.Pin = S2_Pin;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_UP;
+  LL_GPIO_Init(S2_GPIO_Port, &GPIO_InitStruct);
 
   /**/
   GPIO_InitStruct.Pin = S1_Pin;
@@ -302,10 +351,6 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
   GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
   LL_GPIO_Init(LED2_GPIO_Port, &GPIO_InitStruct);
-
-  /* EXTI interrupt init*/
-  NVIC_SetPriority(EXTI0_1_IRQn, 0);
-  NVIC_EnableIRQ(EXTI0_1_IRQn);
 
 /* USER CODE BEGIN MX_GPIO_Init_2 */
 /* USER CODE END MX_GPIO_Init_2 */
