73641f650a1870b91fbe86395512b31ab3ac0ce3
michal-glos-brq <michal.glos.brno@gmail.com>
2024-10-09 16:07:50 +0200

    anrhaeiuighdzfu

---
+++ 03_even_more_epilepsy/Core/Inc/sct.h
@@ -0,0 +1,18 @@
+/*
+ * sct.h
+ *
+ *  Created on: Oct 9, 2024
+ *      Author: xglosm01
+ */
+
+#ifndef INC_SCT_H_
+#define INC_SCT_H_
+
+#include "main.h"
+
+void sct_init();
+void sct_led(uint32_t value);
+void sct_value(uint16_t value);
+
+
+#endif /* INC_SCT_H_ */
